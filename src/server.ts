672984d49da54cf66d0bcf922bee2b944a700409
Samuel Okeke <samwizzy11@gmail.com>
2025-02-06 11:41:14 +0100

    updated netlify.toml

--- src/server.ts
+++ src/server.ts
@@ -13,24 +13,3 @@ export async function netlifyCommonEngineHandler(
 ): Promise<Response> {
   return await render(commonEngine);
 }
-
-// If you want to use "AppEngine" instead - your server.ts file should contain following:
-
-// import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-// import { getContext } from '@netlify/angular-runtime/context';
-
-// const angularAppEngine = new AngularAppEngine();
-
-// export async function netlifyAppEngineHandler(
-//   request: Request
-// ): Promise<Response> {
-//   const context = getContext();
-
-//   const result = await angularAppEngine.handle(request, context);
-//   return result || new Response('Not found', { status: 404 });
-// }
-
-/**
- * The request handler used by the Angular CLI (dev-server and during build).
- */
-// export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
