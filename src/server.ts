bf60b3055ec7940400f57b6ffea3d35a1243629b
Samuel Okeke <samwizzy11@gmail.com>
2025-02-06 11:38:27 +0100

    updated netlify.toml

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,36 @@
- // Error: server.ts doesn't seem to be Netlify compatible and is not known default. Please replace it with Netlify compatible server.ts.
+// Error: server.ts doesn't seem to be Netlify compatible and is not known default. Please replace it with Netlify compatible server.ts.
 
+// It seems like you use "CommonEngine" - for this case your server.ts file should contain following:
 
- // It seems like you use "CommonEngine" - for this case your server.ts file should contain following:
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine';
 
- import { CommonEngine } from '@angular/ssr/node'
- import { render } from '@netlify/angular-runtime/common-engine'
+const commonEngine = new CommonEngine();
 
- const commonEngine = new CommonEngine()
+export async function netlifyCommonEngineHandler(
+  request: Request,
+  context: any
+): Promise<Response> {
+  return await render(commonEngine);
+}
 
- export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-   // Example API endpoints can be defined here.
-   // Uncomment and define endpoints as necessary.
-   // const pathname = new URL(request.url).pathname;
-   // if (pathname === '/api/hello') {
-   //   return Response.json({ message: 'Hello from the API' });
-   // }
+// If you want to use "AppEngine" instead - your server.ts file should contain following:
 
-   return await render(commonEngine)
- }
+// import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+// import { getContext } from '@netlify/angular-runtime/context';
 
+// const angularAppEngine = new AngularAppEngine();
 
- // If you want to use "AppEngine" instead - your server.ts file should contain following:
+// export async function netlifyAppEngineHandler(
+//   request: Request
+// ): Promise<Response> {
+//   const context = getContext();
 
+//   const result = await angularAppEngine.handle(request, context);
+//   return result || new Response('Not found', { status: 404 });
+// }
 
- import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
- import { getContext } from '@netlify/angular-runtime/context'
-
- const angularAppEngine = new AngularAppEngine()
-
- export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-   const context = getContext()
-
-   // Example API endpoints can be defined here.
-   // Uncomment and define endpoints as necessary.
-   // const pathname = new URL(request.url).pathname;
-   // if (pathname === '/api/hello') {
-   //   return Response.json({ message: 'Hello from the API' });
-   // }
-
-   const result = await angularAppEngine.handle(request, context)
-   return result || new Response('Not found', { status: 404 })
- }
-
- /**
-  * The request handler used by the Angular CLI (dev-server and during build).
-  */
- export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+// export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
